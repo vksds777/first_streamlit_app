d937e51a95cc1e454237de7e9c6a8b929a8b4b6e
vksds777 <115366277+vksds777@users.noreply.github.com>
2022-10-09 02:03:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
 import pandas
+import requests
 streamlit.title('My Moms new Healthy Dinner')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')                
+streamlit.header("Fruityvice Fruit Advice!")
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
@@ -10,7 +12,5 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list=my_fruit_list.set_index('Fruit')
 streamlit.multiselect("Pick Some Fruits:",list(my_fruit_list.index),['Apple','Strawberries'])
 streamlit.dataframe(my_fruit_list)
-streamlit.header("Fruityvice Fruit Advice!")
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.jason())
