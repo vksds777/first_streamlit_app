6fddc68c5194d8a936af8a9353632b5f1a20dec8
vksds777 <115366277+vksds777@users.noreply.github.com>
2022-10-09 12:34:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,14 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#my_cur = my_cnx.cursor()
+#snowflake-related functions
+def get_fruit_load_list();
+    with my_cnx.cursor() as my_cur;
+         my_cur.execute("select * from fruit_load_list")
+         return my_cur.fetchall()
+# add a button to load the fruit
+if streamlit.button('Get fruit load list');
+   my_cnx = snowflake.connector.connect()**streamlit.secrets[snowflake])
+   my_data_rows=get_fruit_load_list()
+   streamlit.dataframe(my_data_rows)
